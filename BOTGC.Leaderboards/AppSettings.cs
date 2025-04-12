a66ca527dc78282d2c7a259ac612843f1d9dede3
Simon Parsons <simon@mmaraboustork.co.uk>
2025-04-12 12:21:51 +0100

    add app config

---
+++ BOTGC.Leaderboards/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Services
+{
+    public class AppSettings
+    {
+        public string APIUrlrophyFilePath { get; set; }
+
+        public API AzureFaceApi { get; set; } = new API();
+    }
+
+    public class API
+    {
+        public string XApiKey { get; set; } = "";
+        public string Url { get; set; } = "";
+    }
+}
