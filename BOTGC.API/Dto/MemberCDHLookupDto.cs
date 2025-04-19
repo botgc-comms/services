007addc129d4d938031ea6e0bbd9d53f0e998edd
Simon Parsons <simon@mmaraboustork.co.uk>
2025-04-19 01:17:27 +0100

    update

---
+++ BOTGC.API/Dto/MemberCDHLookupDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BOTGC.API.Dto
+{
+    public class MemberCDHLookupDto: HateoasResource
+    {
+        public string? CdhId { get; set; }
+        public string? HandicapIndex { get; set; }
+        public string? WhsMemberUid { get; set; }
+        public string? ClubName { get; set; }
+        public string? ClubUid { get; set; }
+    }
+}
