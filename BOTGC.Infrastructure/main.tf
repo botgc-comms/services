00506b956bf3872266778e08f376085f833f36b6
Simon Parsons <simon@mmaraboustork.co.uk>
2025-05-25 19:40:56 +0100

    update

--- BOTGC.Infrastructure/main.tf
+++ BOTGC.Infrastructure/main.tf
@@ -295,10 +295,11 @@ output "applicationform_app_name" {
 }
 
 output "cdn_base_url" {
-  value = "https://${azurerm_cdn_endpoint.js_delivery.host_name}"
+  value = "https://${azurerm_cdn_endpoint.js_delivery.fqdn}"
 }
 
 output "membership_embed_js_url" {
-  value = "https://${azurerm_cdn_endpoint.js_delivery.host_name}/js/membership-form-embed.js"
+  value = "https://${azurerm_cdn_endpoint.js_delivery.fqdn}/js/membership-form-embed.js"
 }
+
 
