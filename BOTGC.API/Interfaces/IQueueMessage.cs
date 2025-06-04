8950cea4c2bf116a4d7f7d431f8a3bb47b60f32b
Simon Parsons <simon@mmaraboustork.co.uk>
2025-06-04 13:42:01 +0100

    update

---
+++ BOTGC.API/Interfaces/IQueueMessage.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Storage.Queues.Models;
+
+namespace BOTGC.API.Interfaces
+{
+    public interface IQueueMessage<T>
+    {
+        QueueMessage Message { get; set; }
+        T Payload { get; set; }
+    }
+}
---
+++ BOTGC.API/Models/DeadLetterEnvelope.cs
@@ -0,0 +1,28 @@
+﻿namespace BOTGC.API.Models
+{
+    public class DeadLetterEnvelope<T>
+    {
+        public T? OriginalMessage { get; set; } = default!;
+        public long DequeueCount { get; set; }
+        public DateTime FailedAt { get; set; }
+        public string? ExceptionType { get; set; }
+        public string? ExceptionMessage { get; set; }
+        public string? StackTrace { get; set; }
+
+        public DeadLetterEnvelope() { }
+
+        public DeadLetterEnvelope(T originalMessage, long dequeueCount, DateTime failedAtUtc, Exception? exception)
+        {
+            OriginalMessage = originalMessage;
+            DequeueCount = dequeueCount;
+            FailedAt = failedAtUtc;
+
+            if (exception != null)
+            {
+                ExceptionType = exception.GetType().FullName;
+                ExceptionMessage = exception.Message;
+                StackTrace = exception.StackTrace;
+            }
+        }
+    }
+}
