b4f6744c87b81f83f39d02821864cff9dd673c1f
Simon Parsons <simon@mmaraboustork.co.uk>
2025-06-04 15:28:08 +0100

    update

--- BOTGC.Infrastructure/application_insights.tf
+++ BOTGC.Infrastructure/application_insights.tf
@@ -3,4 +3,47 @@ resource "azurerm_application_insights" "app_insights" {
   location            = azurerm_resource_group.services_api_rg.location
   resource_group_name = azurerm_resource_group.services_api_rg.name
   application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.log_analytics.id
 }
+
+resource "azurerm_monitor_diagnostic_setting" "app_insights_diag" {
+  name                       = "appinsights-diagnostics"
+  target_resource_id         = azurerm_application_insights.app_insights.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+
+  log {
+    category = "Request"
+    enabled  = true
+  }
+
+  log {
+    category = "Trace"
+    enabled  = true
+  }
+
+  log {
+    category = "Exception"
+    enabled  = true
+  }
+
+  log {
+    category = "Dependency"
+    enabled  = true
+  }
+
+  log {
+    category = "Availability"
+    enabled  = true
+  }
+
+  log {
+    category = "PerformanceCounters"
+    enabled  = true
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
+}
+
