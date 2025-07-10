cc5fbd1f35ef7f72e11abfeedc51580b3005a385
Simon Parsons <simon@mmaraboustork.co.uk>
2025-07-10 21:44:56 +0100

    scaleup

--- BOTGC.Infrastructure/service_plan.tf
+++ BOTGC.Infrastructure/service_plan.tf
@@ -3,5 +3,5 @@ resource "azurerm_service_plan" "services_api_asp" {
   location            = azurerm_resource_group.services_api_rg.location
   resource_group_name = azurerm_resource_group.services_api_rg.name
   os_type             = "Linux"
-  sku_name            = "S1"
+  sku_name            = "S2"
 }
