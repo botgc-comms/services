309f6abfb96e40639d1d4d32cfe6e9f7b0bcfba6
Simon Parsons <simon@mmaraboustork.co.uk>
2025-10-12 10:31:42 +0100

    update

---
+++ BOTGC.API/Dto/StockTakeProductSnapshotDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BOTGC.API.Dto
+{
+    public sealed record StockTakeProductSnapshotDto(
+    int StockItemId,
+    string Name,
+    string Unit,
+    string Division,
+    decimal? EstimatedQuantityAtCapture  
+);
+}
