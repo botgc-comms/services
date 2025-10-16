ae65523421ec1f1060d8f518e72a7d014fb20b71
Simon Parsons <simon@mmaraboustork.co.uk>
2025-10-16 08:29:49 +0100

    fix infra

--- BOTGC.Infrastructure/storage.tf
+++ BOTGC.Infrastructure/storage.tf
@@ -6,5 +6,4 @@ resource "azurerm_storage_queue" "membership_applications_queue" {
 resource "azurerm_storage_table" "lookup_table" {
   name                 = "lookup"
   storage_account_name = data.azurerm_storage_account.services_api_sa.name
-  resource_group_name  = data.azurerm_resource_group.services_api_rg.name
 }
